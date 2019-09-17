b412fc360b315072dc7751889d280dd1d1719b17
Nayza <hurtado.nayza@gmail.com>
2019-09-17 16:18:40 -0400

    configurando rutas

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
-    require_once("db.php");
+    /*require_once("db.php");
     $query = "SELECT * FROM contactos";
     $resultado = ObtenerRegistro($query);
-    print_r($resultado);
+    print_r($resultado);*/
+
+    if($_SERVER['REQUEST_METHOD']=='GET'){
+        echo "GET";
+        http_response_code(200);
+    }else if($_SERVER['REQUEST_METHOD']=='POST'){
+        echo "POST";
+        http_response_code(200);
+    }else{
+        http_response_code(405);
+    }
 ?>
