130b9f6826a97a58df0acf94e54fa53ea21f3a92
Nayza <hurtado.nayza@gmail.com>
2019-09-19 10:27:37 -0400

    configurando el metodo post

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
     require_once("utilidades.php");
 
-    if($_SERVER['REQUEST_METHOD']=='GET'){
+    if(isset($_GET['url'])){
         $var = $_GET['url'];
-        $number = intval(preg_replace('/[^0-9]+/', '', $var), 10);
 
-        switch($var){
-            case "contactos":
-                $ans = allContactos();
-                print_r( json_encode($ans));
-            break;
-            case "contactos/$number":
-                $ans = idContactos($number);
-                print_r( json_encode($ans));
-            default;
+        if($_SERVER['REQUEST_METHOD']=='GET'){
+            $number = intval(preg_replace('/[^0-9]+/', '', $var), 10);
+
+            switch($var){
+                case "contactos":
+                    $ans = allContactos();
+                    print_r( json_encode($ans));
+                    http_response_code(200);
+                break;
+                case "contactos/$number":
+                    $ans = idContactos($number);
+                    print_r( json_encode($ans));
+                    http_response_code(200);
+                default;
+            }
+
+        }else if($_SERVER['REQUEST_METHOD']=='POST'){
+            $postBody = file_get_contents("php://input");
+            print_r( json_decode($postBody));
+            http_response_code(200);
+        }else{
+            http_response_code(405);
         }
-
-    }else if($_SERVER['REQUEST_METHOD']=='POST'){
-        echo "POST";
-        http_response_code(200);
     }else{
-        http_response_code(405);
+        //metadata
     }
 ?>
